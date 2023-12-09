ebb5a8bb4f7c73a4c334cada4df8b2fabf9028cd
sousaUA <hsousa@ua.pt>
2023-12-09 12:47:02 +0000

    Static HTML and languageHandler.js init

---
+++ src/templates/languageHandler.js
@@ -0,0 +1,82 @@
+// Function to set text based on selected language
+function setTextBasedOnLanguage(language) {
+    const langTexts = LANGS[language];
+    // Populate HTML elements with values from langTexts...
+    // ...
+    document.getElementById('pageTitle').textContent = langTexts.website_title;
+}
+
+// LANGS object with all your language-specific text
+const LANGS = {
+    'en': {
+        'website_title': 'São Miguel Bus',
+        'title': 'Search a Bus',
+        'subtitle': 'Find the best bus for you.',
+        'From': 'From',
+        'To': 'To',
+        'From_placeholder': 'Type your origin...',
+        'To_placeholder': 'Type your destination...',
+        'Day': 'Day Of the Week',
+        'Weekday': 'Weekday',
+        'Saturday': 'Saturday',
+        'Sunday': 'Sunday/Holiday',
+        'Time': 'What Time?',
+        'Optional': 'Optional',
+        'Search': 'Search for a Bus',
+        'warning': 'Only direct routes are shown',
+        'No_routes1': 'No routes found from',
+        "No_routes2": "to",
+        'No_routes_subtitle': 'Please be careful with the spelling. Choose one Stop from the options displayed.',
+        'Search_menu': 'Search',
+        'Contact': 'Contact',
+        'card_title': 'Get In Touch',
+        'contact_title': 'Found anything wrong?',
+        'contact_subtitle': 'We\'re open for any suggestion or just to have a chat',
+        'contact_name': 'Name',
+        'contact_email': 'Email',
+        'contact_subject': 'Subject',
+        'contact_message': 'Message',
+        'contact_button': 'Send Message Now',
+        'support': "Support the",
+        'support_bold': "Developer",
+        'route_warning': 'Information taken from public sources provided by bus companies',
+        'info_warning': "This website is not affiliated with any company or organization. The present schedule was taken from public documents made available by the bus companies. Some routes/hours may be outdated!",
+    },
+    'pt': {
+        'website_title': 'Autocarros São Miguel',
+        'title': 'Procura um Autocarro',
+        'subtitle': 'Encontra a melhor rota para ti.',
+        'From': 'Partida',
+        'To': 'Destino',
+        'From_placeholder': 'Escolhe o teu ponto de partida...',
+        'To_placeholder': 'Escolhe o teu destino...',
+        'Day': 'Dia da Semana',
+        'Weekday': 'Dia útil',
+        'Saturday': 'Sábado',
+        'Sunday': 'Domingo/Feriado',
+        'Time': 'A que horas?',
+        'Optional': 'Opcional',
+        'Search': 'Pesquisar autocarros',
+        'warning': 'Apenas são apresentadas rotas diretas',
+        'No_routes1': 'Não foram encontradas rotas entre',
+        "No_routes2": "e",
+        'No_routes_subtitle': 'Verifica se as paragens estão corretamente escritas ou escolhe uma das opções apresentadas.',
+        'Search_menu': 'Pesquisar',
+        'Contact': 'Contatar',
+        'card_title': 'Entre em contato',
+        'contact_title': 'Encontrou algo errado?',
+        'contact_subtitle': 'Estamos abertos para qualquer sugestão ou apenas para conversar',
+        'contact_name': 'Nome',
+        'contact_email': 'Email',
+        'contact_subject': 'Assunto',
+        'contact_message': 'Mensagem',
+        'contact_button': 'Enviar Mensagem',
+        'support': "Apoia o",
+        'support_bold': "Criador",
+        'route_warning': 'Informação retirada de fontes públicas disponibilizadas pelas empresas de autocarros',
+        "info_warning": "Este site não é afiliado a nenhuma empresa ou organização. O horário usado foi retirado de documentos públicos disponibilizados pelas empresas de autocarros. Alguns horários podem estar desatualizados!"
+    }
+};
+
+// Call this function on page load or when the user changes the language
+setTextBasedOnLanguage('pt'); // Default to English, for example
