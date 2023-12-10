dc308f684d1e3255c6edf065506e77edc8b88b00
sousaUA <hsousa@ua.pt>
2023-12-10 18:33:30 +0000

    Get Routes from the API

--- src/templates/apiHandler.js
+++ src/templates/apiHandler.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     fetchAndPopulateStops();
+    const searchBtn = document.getElementById('btnSubmit')
+    searchBtn.addEventListener('click', function(event) {
+        event.preventDefault();  // Prevents the default form submission action
+
+        const origin = document.getElementById('origin').value;
+        const destination = document.getElementById('destination').value;
+        const day = document.getElementById('day').value;
+        const time = document.getElementById('time').value;
+
+        // Call your function here, passing the origin and destination
+        searchRoutes(origin, destination, day, time);
+    });
 });
 
 function fetchAndPopulateStops() {
@@ -24,4 +36,153 @@ function fetchAndPopulateStops() {
         .catch(error => {
             console.error('Error fetching stops:', error);
         });
+}
+
+function searchRoutes(origin, destination, day, time) {
+    console.log("Searching routes from", origin, "to", destination);
+
+    // Hide the routes container and the no routes message
+    document.getElementById('routesContainer').style.display = 'none';
+    document.getElementById('noRoutesMessage').style.display = 'none';
+
+
+    // TODO: format the origin and destination strings to remove spaces and special characters
+    const parameters = getUrlParameters(origin, destination, day, time);
+    const url = 'https://saomiguelbus-api.herokuapp.com/api/v1/route?origin=' + parameters.origin 
+    + '&destination=' + parameters.destination 
+    + '&day=' + parameters.day 
+    + '&start=' + parameters.time
+    console.log('url: ', url);
+    fetchAndDisplayRoutes(url, parameters.origin);
+    // TODO: Add the post request to the API here
+}
+
+function fetchAndDisplayRoutes(url, originStop) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.length > 0) {
+                console.log('Displaying routes...');
+                displayRoutes(data, originStop);
+            } else {
+                displayNoRoutesMessage();
+            }
+        })
+        .catch(error => console.error('Error fetching routes:', error));
+}
+
+function displayNoRoutesMessage() {
+    const noRoutesDiv = document.getElementById('noRoutesMessage');
+    noRoutesDiv.innerHTML = '<div class="container"><div class="row"><div class="col-xs-12"><h3>No routes found</h3><p>Subtitle or additional info here.</p></div></div></div>';
+    noRoutesDiv.style.display = 'block';
+}
+
+function displayRoutes(routes, originStop) {
+    const routesContainer = document.getElementById('routesContainer');
+    routesContainer.innerHTML = ''; // Clear previous content
+
+    console.log('There are', routes.length, 'routes to display');
+
+    // Sort routes by origin time
+    routes.sort((a, b) => {
+        aStopsString = a.stops;
+        const aValidJsonString = aStopsString.replace(/'/g, '"');
+        // Parse the string to a JavaScript object
+        const aStopsObj = JSON.parse(aValidJsonString);
+
+        bStopsString = b.stops;
+        const bValidJsonString = bStopsString.replace(/'/g, '"');
+        // Parse the string to a JavaScript object
+        const bStopsObj = JSON.parse(bValidJsonString);
+
+        const aTime = aStopsObj[originStop];
+        const bTime = bStopsObj[originStop];
+        return aTime.localeCompare(bTime);
+    });
+
+    routes.forEach(route => {
+        const routeDiv = document.createElement('div');
+        routeDiv.className = 'container card w-100 center';
+        routeDiv.style.cssText = 'margin-top: 30px; border-radius: 8px; padding: 10px;';
+
+        let stopsHtml = '';
+        stopsString = route.stops;
+        // Convert to valid JSON format by replacing single quotes with double quotes
+        const validJsonString = stopsString.replace(/'/g, '"');
+
+        // Parse the string to a JavaScript object
+        const stopsObj = JSON.parse(validJsonString);
+
+        // Iterate over the object
+        for (const [stop, time] of Object.entries(stopsObj)) {
+            stopsHtml += `<b>${stop}</b>: ${time} <br />`;
+        }
+
+        // Get the time for the origin stop
+        const originTime = stopsObj[originStop];
+
+        routeDiv.innerHTML = `
+            <div class="row">
+                <div class="col"></div>
+                <div class="col">
+                    <p class="card-text center-center" style="font-size: 20px; margin-left: 10px; width: 100%; text-align: center">${route.origin}</p>
+                </div>
+                <div class="col">
+                    <p class="card-text" style="float:right; font-weight:bold; font-size: 20px; text-align: center; margin: auto; padding:10px">${originTime}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col" style="margin: auto">
+                    <h5 class="card-title" style="font-weight: bold; margin-left: 10px; color: black;">${route.route}</h5>
+                </div>
+                <div class="col">
+                    <p class="card-text center-center" style="font-size: 20px; font-weight: bold; width: 100%; text-align: center">&#8595</p>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col">
+                    <p class="card-text center-center" style="font-size: 20px; margin-left: 10px; width: 100%; text-align: center">${route.destination}</p>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input class="spoilerbutton" type="button" value="+" onclick="this.value=this.value=='+'?'-':'+';">
+                    <div class="spoiler" style="display: none;">${stopsHtml}</div>
+                </div>
+            </div>
+            ${route.information!='None' ? `<text style="color: red">${route.information}</text>` : ''}
+        `;
+
+        const spoilerButton = routeDiv.querySelector('.spoilerbutton');
+        const spoilerContent = routeDiv.querySelector('.spoiler');
+
+        spoilerButton.addEventListener('click', function() {
+            if (spoilerContent.style.display === 'none' || !spoilerContent.style.display) {
+                spoilerContent.style.display = 'block';
+                spoilerButton.value = '-';
+            } else {
+                spoilerContent.style.display = 'none';
+                spoilerButton.value = '+';
+            }
+        });
+
+        routesContainer.appendChild(routeDiv);
+    });
+
+    routesContainer.style.display = 'block';
+}
+
+function getUrlParameters(origin, destination, day, time) {
+    const parameters = {
+        'origin': origin,
+        'destination': destination,
+        'day': day,
+        'time': time
+    };
+    // 1 -> weekday, 2 -> saturday, 3 -> sunday
+    parameters.day = parameters.day == 1 ? 'weekday' : parameters.day == 2 ? 'saturday' : 'sunday';
+    return parameters;
 }
